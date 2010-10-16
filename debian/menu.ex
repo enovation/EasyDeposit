6bac95a4afa9e207cdde4cc3acc63ebb53840b34
Tomasz Muras <tm@enovation.ie>
2010-10-16 16:36:49 +0100

    Configuring & de-configuring apache

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ndlrdeposit):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ndlrdeposit" command="/usr/bin/ndlrdeposit"
--- debian/ndlrdeposit.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ndlrdeposit package
-#
-0 4	* * *	root	[ -x /usr/bin/ndlrdeposit_maintenance ] && /usr/bin/ndlrdeposit_maintenance
